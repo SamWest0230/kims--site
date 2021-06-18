787164fb1217ced274cbe9384fbcc557acdcd159
sam west <samwest0230@gmail.com>
2021-06-18 13:58:49 -0400

    prepare for production

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const booking = require("./booking.js")
 const cors = require("cors");
 require('dotenv').config()
 const PORT = process.env.PORT || 8080;
+const path = require('path');
 
 const limiter = rateLimit({
     max: 100,
@@ -17,10 +18,33 @@ const limiter = rateLimit({
 app.use(limiter);
 app.use(helmet());
 app.use(morgan('tiny'));
-app.use(cors());
+
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.heroku...']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/booking", booking) 
 
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 
 app.listen(PORT, () => {
