1f70650ba2150343198b1779c59e9a4c5c5181a7
sam west <samwest0230@gmail.com>
2021-06-18 14:55:11 -0400

    fixed

--- server.js
+++ server.js
@@ -38,13 +38,13 @@ app.use("/booking", booking)
 
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(express.static(__dirname, 'client/build', 'index.html'));
-  });
-}
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
 
 
 app.listen(PORT, () => {
