39024e7aaf405d7b7699af6123b9ebbcfbf4f2ec
sam west <samwest0230@gmail.com>
2021-06-18 14:47:06 -0400

    deployment bug fix

--- server.js
+++ server.js
@@ -39,10 +39,10 @@ app.use("/booking", booking)
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(express.static(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.js'));
+    res.sendFile(express.static(__dirname, 'client/build', 'index.html'));
   });
 }
 
