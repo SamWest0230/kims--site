d5bc6603232cdb48d0354e8a1ef5fe0ffd0b52ab
sam west <samwest0230@gmail.com>
2021-07-20 18:53:36 -0400

    added OAuth2 for nodemailer & Gmail

--- booking.js
+++ booking.js
@@ -19,10 +19,10 @@ const createTransporter = async () => {
     refresh_token: process.env.REFRESH_TOKEN
   });
 };
-const accessToken = await new Promise((resolve, reject) => {
+const accessToken = new Promise((resolve, reject) => {
   oauth2Client.getAccessToken((err, token) => {
     if (err) {
-      reject("Failed to create access token :(");
+      reject("Failed to create access token");
     }
     resolve(token);
   });
