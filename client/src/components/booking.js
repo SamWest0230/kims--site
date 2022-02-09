284fc05786ff61e5bcebc1e2dcc9112ab26e4f4a
sam west <samwest0230@gmail.com>
2022-02-09 15:00:24 -0500

    updated env

--- client/src/components/booking.js
+++ client/src/components/booking.js
@@ -20,7 +20,6 @@ class Booking extends React.Component{
 
     }
     handleChange = (e) => {
-      console.log(e.target.value)
         this.setState({
           [e.target.name]: e.target.value
         })
